45258b8e26812821a70e6de39ebabd1256294035
mateoGom <matemgm@hotmail.com>
2020-11-30 21:58:45 +0100

    transforming data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -18,7 +18,14 @@ class Blog extends Component {
         axios
       .get("https://jsonplaceholder.typicode.com/posts")
       .then((response) => {
-        this.setState({posts:response.data})
+          const posts=response.data.slice(0,4)
+          const updatedPost=posts.map(post=>{
+              return{
+                  ...post,
+                  author:'Max'
+              }
+          })
+        this.setState({posts:updatedPost})
       });
     }
 
@@ -26,7 +33,7 @@ class Blog extends Component {
   render() {
 
     const posts=this.state.posts.map(post=>{
-        return <Post key={post.id} title={post.title}/>
+        return <Post key={post.id} title={post.title} author={post.author}/>
     })
 
     return (
