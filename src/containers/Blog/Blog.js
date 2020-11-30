c837b7615dcc739386e08fa0b6991d73213af762
mateoGom <matemgm@hotmail.com>
2020-11-30 22:21:17 +0100

    make a post selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,44 +6,46 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 import axios from "axios";
 
-
-
 class Blog extends Component {
-    state={
-        posts:[]
-    }
+  state = {
+    posts: [],
+    selectedPostId: null,
+  };
 
-
-    componentDidMount () {
-        axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => {
-          const posts=response.data.slice(0,4)
-          const updatedPost=posts.map(post=>{
-              return{
-                  ...post,
-                  author:'Max'
-              }
-          })
-        this.setState({posts:updatedPost})
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      const posts = response.data.slice(0, 4);
+      const updatedPost = posts.map((post) => {
+        return {
+          ...post,
+          author: "Max",
+        };
       });
-    }
+      this.setState({ posts: updatedPost });
+    });
+  }
 
+  postSelectedHandler=(id)=> {
+    this.setState({ selectedPostId: id });
+  }
 
   render() {
-
-    const posts=this.state.posts.map(post=>{
-        return <Post key={post.id} title={post.title} author={post.author}/>
-    })
+    const posts = this.state.posts.map((post) => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
+    });
 
     return (
       <div>
-        <section className="Posts">
-          
-          {posts}
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId}/>
         </section>
         <section>
           <NewPost />
